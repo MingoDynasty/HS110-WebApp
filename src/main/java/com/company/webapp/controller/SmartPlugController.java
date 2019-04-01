1dee5c13015521e7c578fe92c8ff41b36a39d6b3
Mingo <sourcetree@mingodynasty.com>
2019-03-31 20:43:10 -0400

    Some reorganization.

--- src/main/java/com/company/webapp/controller/SmartPlugController.java
+++ src/main/java/com/company/webapp/controller/SmartPlugController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.company.webapp.exception.SmartPlugNotFoundException;
 import com.company.webapp.model.SmartPlug;
+import com.company.webapp.service.SmartPlugService;
 
 @RestController
+@RequestMapping("v1")
 public class SmartPlugController {
   private static final Logger logger = LoggerFactory.getLogger(SmartPlugController.class);
 
--- src/main/java/com/company/webapp/service/SmartPlugService.java
+++ src/main/java/com/company/webapp/service/SmartPlugService.java
@@ -1,4 +1,4 @@
-package com.company.webapp.controller;
+package com.company.webapp.service;
 
 import java.util.List;
 import java.util.Optional;
